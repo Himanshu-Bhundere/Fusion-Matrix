88d9e09491694dea540740c4917bb2d34a7fb5bb
ATP_429 <atpgaming123@gmail.com>
2022-01-18 20:14:40 +0530

    Change login structure to match table structure, remove unnecessary files
    - Since staff_credentials only holds staff id, username, pass, and staff type is stored in staff_details, we needed to change login code
      Also, registerStaff now creates an account for staff in staff_credentials, and passing username is necessary
    - Removed unnecessary logout.php, invoice.php, etc which can now be called using js functions

--- Server/register.php
+++ Server/register.php
@@ -1,10 +1,8 @@
 <?php
     require 'server.php';
 
-	$username = $_GET['username'];
-	$password = $_GET['password'];
-    $staff_type = $_GET['staff_type'];
     $server = new Server;
     $server->connect();
-    echo $server->register($username, $password, $staff_type);
+
+    $server->register_staff(array("username"=>$_GET['username'], "staff_type"=>$_GET['staff_type']));
 ?>
--- Server/register_customer.php
+++
@@ -1,7 +0,0 @@
-<?php
-	require 'server.php';
-	$server = new Server;
-	$server->connect();
-	$roomno = $_GET['room_no'];
-	$server->register_customer($roomno, array("firstname" => "Aaryash", "lastname" => "18"));
-?>
