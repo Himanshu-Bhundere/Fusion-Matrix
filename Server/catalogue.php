5407fa7577c7ce080953cc44bdfdecd6d0dd9de5
Himanshu-Bhundere <himanshubhundere@gmail.com>
2022-10-20 16:17:34 +0530

    .

--- Server/catalogue.php
+++
@@ -1,6 +0,0 @@
-<?php
-	require 'server.php';
-	$server = new Server;
-	$server->connect();
-	$server->catalogue(array('Regular'=>'5000', 'Suite'=>'7000', 'Prime'=>'9500', 'Deluxe'=>'12000'));
-?>
--- Server/register.php
+++
@@ -1,8 +0,0 @@
-<?php
-    require 'server.php';
-
-    $server = new Server;
-    $server->connect();
-
-    $server->register_staff(array("username"=>$_GET['username'], "staff_type"=>$_GET['staff_type']));
-?>
